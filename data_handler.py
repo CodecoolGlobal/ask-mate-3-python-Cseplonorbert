ac51224fb78b6792c35b6d42c955321e012866ca
markberencsi <markberencsi@gmail.com>
2021-10-19 15:24:47 +0200

    Add_new_question implemented

--- data_handler.py
+++ data_handler.py
@@ -1,5 +1,5 @@
 import csv
-from datetime import date
+from datetime import date, datetime
 
 HEADERS = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
 
@@ -20,22 +20,38 @@ def convert_dates(questions):
 
 
 def get_date(submission_time):
-    post_date = date.fromtimestamp(submission_time)
+    post_date = datetime.fromtimestamp(submission_time)
     return post_date
 
 
-def write_questions_to_file(file_name="question.csv"):
-    pass
+def write_questions_to_file(questions, file_name="question.csv"):
+    with open(file_name, "w+") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=HEADERS)
+        writer.writeheader()
+        writer.writerows(questions)
 
 
 def get_question_by_id(question_id):
     pass
 
 
+def date_time_to_timestamp(date_time):
+    submission_time = datetime.timestamp(date_time)
+    return submission_time
+
+
 def add_question(question):
-    pass
+    questions = get_questions()
+    new_id = len(questions)+1
+    question['id'] = new_id
+    question['view_number'] = 0
+    question['vote_number'] = 0
+    today_date = datetime.now()
+    question['submission_time'] = date_time_to_timestamp(today_date)
+    questions.append(question)
+    write_questions_to_file(questions)
 
-
+    
 def get_answers(file_name = "answers.csv"):
     pass
 
