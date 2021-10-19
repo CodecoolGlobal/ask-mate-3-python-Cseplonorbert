beddcd42aba11e4a6489aee4b1f124b875fc3e0b
Cséplő Norbert <cseplo.norbert.95@gmail.com>
2021-10-19 14:41:14 +0200

    get_questions bug fixed

--- data_handler.py
+++ data_handler.py
@@ -1,6 +1,27 @@
+import csv
+from datetime import date
+
+HEADERS = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
+
 
 def get_questions(file_name="question.csv"):
-    pass
+    with open(file_name, "r", newline="") as csv_file:
+        reader = csv.DictReader(csv_file)
+        questions = list(reader)
+        questions = convert_dates(questions)
+        return questions
+
+
+def convert_dates(questions):
+    for question in questions:
+        submission_time = question["submission_time"]
+        question["submission_time"] = get_date(int(submission_time))
+    return questions
+
+
+def get_date(submission_time):
+    post_date = date.fromtimestamp(submission_time)
+    return post_date
 
 
 def write_questions_to_file(file_name="question.csv"):
