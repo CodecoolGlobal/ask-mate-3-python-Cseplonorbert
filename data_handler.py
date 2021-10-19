86f7ae1fb071123db4872c3fbe7227092db9adda
Cséplő Norbert <cseplo.norbert.95@gmail.com>
2021-10-19 20:15:45 +0200

    date_bug fixed

--- data_handler.py
+++ data_handler.py
@@ -1,5 +1,5 @@
 import csv
-from datetime import date, datetime
+from date_handler import get_today_date_to_time_stamp
 
 HEADERS = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
 
@@ -8,20 +8,7 @@ def get_questions(file_name="question.csv"):
     with open(file_name, "r", newline="") as csv_file:
         reader = csv.DictReader(csv_file)
         questions = list(reader)
-        questions = convert_dates(questions)
         return questions
-
-
-def convert_dates(questions):
-    for question in questions:
-        submission_time = question["submission_time"]
-        question["submission_time"] = get_date(int(submission_time))
-    return questions
-
-
-def get_date(submission_time):
-    post_date = datetime.fromtimestamp(submission_time)
-    return post_date
 
 
 def write_questions_to_file(questions, file_name="question.csv"):
@@ -35,19 +22,13 @@ def get_question_by_id(question_id):
     pass
 
 
-def date_time_to_timestamp(date_time):
-    submission_time = datetime.timestamp(date_time)
-    return submission_time
-
-
 def add_question(question):
     questions = get_questions()
     new_id = len(questions)+1
     question['id'] = new_id
     question['view_number'] = 0
     question['vote_number'] = 0
-    today_date = datetime.now()
-    question['submission_time'] = date_time_to_timestamp(today_date)
+    question['submission_time'] = get_today_date_to_time_stamp()
     questions.append(question)
     write_questions_to_file(questions)
 
