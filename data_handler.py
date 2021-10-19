b1a40853cdf081885dce51090a019729cc59b7bf
ekovattaa <ekovacsattila@gmail.com>
2021-10-19 10:00:36 +0200

    Implement write_question_to_file and add_question functions

--- data_handler.py
+++ data_handler.py
@@ -3,5 +3,13 @@ def get_questions(file_name="question.csv"):
     pass
 
 
+def write_questions_to_file(file_name="question.csv"):
+    pass
+
+
 def get_question_by_id(question_id):
+    pass
+
+
+def add_question(question):
     pass
