bc4dcf65676c245ec5f558adf4d3d35488a3a225
Péter Markó <peter.marko1089@gmail.com>
2021-10-20 10:35:26 +0200

    display_question implemented

--- data_handler.py
+++ data_handler.py
@@ -1,44 +1,50 @@
 import csv
 from date_handler import get_today_date_to_time_stamp
 
-HEADERS = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
+QUESTION_HEADERS = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
+ANSWERS_HEADERS = ['id','submission_time','vote_number','question_id','message','image']
 
-
-def get_questions(file_name="question.csv"):
+def get_data_from_file(file_name="question.csv"):
     with open(file_name, "r", newline="") as csv_file:
         reader = csv.DictReader(csv_file)
-        questions = list(reader)
-        return questions
+        data = list(reader)
+        return data
 
 
-def write_questions_to_file(questions, file_name="question.csv"):
+def write_data_to_file(data,headers, file_name="question.csv"):
     with open(file_name, "w+") as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=HEADERS)
+        writer = csv.DictWriter(csv_file, fieldnames=headers)
         writer.writeheader()
-        writer.writerows(questions)
+        writer.writerows(data)
 
 
 def get_question_by_id(question_id):
-    pass
+    questions = get_data_from_file()
+    for question in questions:
+        if question['id'] == question_id:
+            return question
+
 
 
 def add_question(question):
-    questions = get_questions()
+    questions = get_data_from_file()
     new_id = len(questions)+1
     question['id'] = new_id
     question['view_number'] = 0
     question['vote_number'] = 0
     question['submission_time'] = get_today_date_to_time_stamp()
-    questions.append(question)
-    write_questions_to_file(questions)
-
-    
-def get_answers(file_name = "answers.csv"):
-    pass
+    questions.append()
+    write_data_to_file(questions,QUESTION_HEADERS)
 
 
-def write_answers_to_file(file_name = "answers.csv"):
-    pass
+def get_answers_by_question_id(question_id):
+    answers = get_data_from_file('answer.csv')
+    related_answers = []
+    for answer in answers:
+        if answer['question_id'] == question_id:
+            related_answers.append(answer)
+    return related_answers
+
 
 
 def add_new_answer(answer):
