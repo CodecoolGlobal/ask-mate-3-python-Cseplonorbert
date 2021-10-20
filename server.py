8b8d7682940b46aa097ae870b87d1c899e071685
Cséplő Norbert <cseplo.norbert.95@gmail.com>
2021-10-20 23:02:23 +0200

    delete and edit question bugs fixed

--- server.py
+++ server.py
@@ -1,26 +1,58 @@
-from flask import Flask, render_template
-from bonus_questions import SAMPLE_QUESTIONS
+from flask import Flask, render_template, request, redirect
+import data_handler
+from date_handler import convert_timestamps_to_date
+from werkzeug.utils import secure_filename
+import os
+
 
 app = Flask(__name__)
+app.config["IMAGE_UPLOADS"] = f"{os.getcwd()}/static/images"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPG", "PNG"]
 
 
-@app.route("/bonus-questions")
-def main():
-    return render_template('bonus_questions.html', questions=SAMPLE_QUESTIONS)
+def allowed_image(filename):
+    if "." not in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
+
+@app.route("/", methods=["GET"])
+@app.route("/list", methods=["GET"])
+def main_page():
+    questions = data_handler.get_data_from_file()
+    questions = convert_timestamps_to_date(questions)
+    return render_template("index.html", questions=questions)
 
 
 @app.route("/question/<question_id>")
 def display_question(question_id):
+    data_handler.increase_view(question_id)
     question = data_handler.get_question_by_id(question_id)
-    return render_template("question.html", question=question)
+    answers = data_handler.get_answers_by_question_id(question_id)
+    answers = convert_timestamps_to_date(answers)
+    return render_template("question.html", question=question, answers=answers)
 
 
 @app.route("/add_question", methods=["GET", "POST"])
 def add_question():
     if request.method == "POST":
         question = dict()
+        if request.files:
+            image = request.files["image"]
+            if allowed_image(image.filename):
+                filename = secure_filename(image.filename)
+
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+                question["image"] = f"images/{image.filename}"
+
         question['title'] = request.form['title']
-        question['message'] = request.form['message']   
+        question['message'] = request.form['message']
         data_handler.add_question(question)
         return redirect('/list')
     elif request.method == "GET":
@@ -29,17 +61,29 @@ def add_question():
 
 @app.route("/question/<question_id>/new_answer", methods=["POST"])
 def add_new_answer(question_id):
+    answer = dict()
     if request.method == "POST":
-        answer = request.form
-        answer["answer_id"]=question_id
+        if request.files:
+            image = request.files["image"]
+            if allowed_image(image.filename):
+                filename = secure_filename(image.filename)
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+                answer["image"] = f"images/{image.filename}"
+        answer["message"] = request.form["message"]
+        answer["question_id"] = question_id
         data_handler.add_new_answer(answer)
-    return redirect("/question/<question_id>")
+    return redirect(f"/question/{question_id}")
 
 
 @app.route("/question/<question_id>/delete", methods=["GET"])
 def delete_question(question_id):
     if request.method == 'GET':
-        data_handler.delete_question_by_id(question_id)
+        answers = data_handler.get_answers_by_question_id(question_id)
+        if answers:
+            for answer in answers:
+                answer_id = answer['id']
+                data_handler.delete_by_id(answer_id, 'answer')
+        data_handler.delete_by_id(question_id, "question")
         return redirect('/list')
 
 
@@ -47,43 +91,55 @@ def delete_question(question_id):
 def edit_question(question_id):
     question = data_handler.get_question_by_id(question_id)
     if request.method == "POST":
-        question = request.form
+        question['title'] = request.form['title']
+        question['message'] = request.form['message']
+        if request.files:
+            image = request.files["image"]
+            if allowed_image(image.filename):
+                filename = secure_filename(image.filename)
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+                question["image"] = f"images/{image.filename}"
         data_handler.edit_question(question)
-        return redirect("/question/<question_id>")
+        return redirect(f"/question/{question_id}")
     return render_template("edit_question.html", question=question)
 
 
-@app.route("/answer/<answer_id>/delete", methods = ["GET"])
+@app.route("/answer/<answer_id>/delete", methods=["GET"])
 def delete_answer(answer_id):
-    data_handler.delete_answer(answer_id)
-    return redirect("/question/<question_id>")
+    question_id = data_handler.get_question_id_by_answer_id(answer_id)
+    data_handler.delete_by_id(answer_id, "answer")
+    return redirect(f"/question/{question_id}")
 
 
 @app.route("/question/<question_id>/vote_up", methods=["GET"])
 def question_vote_up(question_id):
-    data_handler.vote_question(question_id, "up")
+    data_handler.vote_dict(question_id, "up", "question")
     return redirect(f"/question/{question_id}")
 
 
 @app.route("/question/<question_id>/vote_down", methods=["GET"])
 def question_vote_down(question_id):
-    data_handler.vote_question(question_id, "down")
+    data_handler.vote_dict(question_id, "down", "question")
     return redirect(f"/question/{question_id}")
 
 
 @app.route("/answer/<answer_id>/vote_up", methods=["GET"])
 def answer_vote_up(answer_id):
     question_id = data_handler.get_question_id_by_answer_id(answer_id)
-    data_handler.vote_answer(answer_id, "up")
+    data_handler.vote_dict(answer_id, "up", "answer")
     return redirect(f"/question/{question_id}")
 
 
 @app.route("/answer/<answer_id>/vote_down", methods=["GET"])
 def answer_vote_down(answer_id):
     question_id = data_handler.get_question_id_by_answer_id(answer_id)
-    data_handler.vote_answer(answer_id, "down")
+    data_handler.vote_dict(answer_id, "down", "answer")
     return redirect(f"/question/{question_id}")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True
+    )
