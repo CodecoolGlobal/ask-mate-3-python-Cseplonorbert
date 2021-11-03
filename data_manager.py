76c76013802a69618c4992f328c7f4318faa2881
Péter Markó <peter.marko1089@gmail.com>
2021-11-03 10:53:27 +0100

    feat: add data_manager/add question function

--- data_manager.py
+++ data_manager.py
@@ -4,3 +4,14 @@ from psycopg2 import sql
 from psycopg2.extras import RealDictCursor
 
 import database_common
+
+
+@database_ommon.connection_handler
+def add_question(cursor, submission_time, view_number, vote_number, title, message, image):
+    query = f"""
+            INSERT INTO question
+            VALUES('{submission_time}', '{view_number}', '{vote_number}', '{title}','{message}', '{image}')
+            """
+    cursor.execute(query)
+    return cursor.fetchall()
+
