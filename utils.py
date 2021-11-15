d0995eb18234729b3e825de7daf727f8853b9b8e
Péter Markó <peter.marko1089@gmail.com>
2021-11-15 17:01:47 +0100

    feat: add utils/ check_hash_password and generate hash_password function

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 
 def allowed_image(filename, allowed_extensions):
@@ -10,3 +13,13 @@ def allowed_image(filename, allowed_extensions):
         return True
     else:
         return False
+
+
+def generate_hash_password(plain_text_password):
+    hashed_password = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+    return hashed_password
+
+
+def check_hash_password(hashed_password, plain_text_password):
+    checked_password = bcrypt.check_password_hash(hashed_password, plain_text_password)
+    return checked_password
