41e7790610e64a2032ef32ceeffa51f98c20cd1a
Cseplonorbert <cseplo.norbert.95@gmail.com>
2021-11-16 10:00:32 +0100

    fix: utils.py/ fix password bug

--- utils.py
+++ utils.py
@@ -13,11 +13,11 @@ def allowed_image(filename, allowed_extensions):
         return False
 
 
-def generate_hash_password(plain_text_password):
-    hashed_password = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
-    return hashed_password
+def hash_password(plain_text_password):
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
 
 
-def check_hash_password(hashed_password, plain_text_password):
-    checked_password = bcrypt.check_password_hash(hashed_password, plain_text_password)
-    return checked_password
+def verify_password(plain_text_password, hashed_password):
+    hashed_bytes_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
